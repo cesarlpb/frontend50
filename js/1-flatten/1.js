c2001fa891622b36456eded9e628c4647a65ed8d
César <cesarlpb@gmail.com>
2023-09-07 20:14:26 +0200

    Solución parcial - no tiene en cuenta objetos

--- js/1-flatten/1.js
+++ js/1-flatten/1.js
@@ -1,15 +1,74 @@
+function  isPrimitive(variable){
+  if( typeof variable == 'function'){
+    throw new Error('No se admiten funciones como input')
+  }
+  if(variable === null){
+    return true; // null bugeado. Más info: https://2ality.com/2013/10/typeof-null.html
+  }
+  // Los tipos considerados primitivos además del null:
+  tiposPrimitivos = ["undefined", "number", "boolean", "string"]
+  if( tiposPrimitivos.includes((typeof variable).toString()) ){
+    return true
+  }
+  return false
+}
+function flattenArray(arr){
+  let flattenedArray = []
+  for(let i = 0; i < arr.length; i++){
+    let item = arr[i];
+    if(Array.isArray(item) && item.length == 0){
+      continue
+    }
+    else if(Array.isArray(item)){
+      let temp = flattenArray(item) // item es un array -> recursión
+      temp.forEach(e => { flattenedArray.push(e) })
+    }
+    else{
+      flattenedArray.push(item)
+    }
+  }
+  return flattenedArray
+}
 function flatten(input){
   // Necesitamos saber si el input es tipo primitivo de JS
-
-  // Salida de objetos
-
-  // Salida de arrays
-  
-  return input
+    let isPrimitiveInput = isPrimitive(input) // true o false
+    // funciones no se admiten como input
+    if(isPrimitiveInput){
+      return input
+    }else{
+      if(typeof input === 'object' &&
+      !Array.isArray(input) &&
+      input !== null){
+        // Salida de objetos
+        return 'object'
+      } else if(Array.isArray(input)){
+        // Salida de arrays 
+        let flattenedArray = []
+        flattenArray(input)
+        return flattenedArray
+      }else{
+        throw new Error('Tipo de variable no válido' + typeof variable)
+      }
+    }
 }
 
 // Algunos casos de uso conocido:
-console.log("input: 1", "output: " + flatten(1)) // 1
-console.log("input: []", "output: " + flatten([])) // []
+console.group("Casos de input primitivo:")
+  console.log("input: null,", "output: " + flatten(null)) // null
+  console.log("input: undefined,", "output: " + flatten(undefined)) // undefined
+  console.log("input: 1,", "output: " + flatten(1)) // 1
+  console.log("input: 'text',", "output: " + flatten('text')) // 'text'
+console.groupEnd()
+
+let objeto = {
+  "a": "algo",
+  "b": {"c": "más cosas"}
+}
+
+console.group("Casos de input no primitivo:")
+  console.log("input: [],", "output: " + flatten([])) // []
+  console.log("input: [1, 2, 3],", "output: " + flatten([1, 2, 3])) // []
+  console.log("input: {},", "output: " + flatten({})) // {}
+  console.log("input: ," + JSON.stringify(objeto), "output: " + flatten(objeto)) // {}
+console.groupEnd()
 // console.log("input: 1", "output: " + flatten([1, 2, [3, 4, [], 5]])) // [1, 2, 3, 4, 5]
-console.log("input: {}", "output: " + flatten({})) // {}
